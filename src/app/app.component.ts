c93c377af6164a6c91b801cd4754c8fe9904c170
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-01-13 12:03:06 +0530

    dasfrsdfg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { MyComponentComponent } from "./folder/my-component/my-component.component";
+import { TopnavbarComponent } from './folder/topnavbar/topnavbar.component';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, MyComponentComponent ,TopnavbarComponent ]
 })
 export class AppComponent {
   title = 'my-new-project';
---
+++ src/app/dist/app.component.js
@@ -0,0 +1,30 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppComponent = void 0;
+var core_1 = require("@angular/core");
+var common_1 = require("@angular/common");
+var router_1 = require("@angular/router");
+var my_component_component_1 = require("./folder/my-component/my-component.component");
+var topnavbar_component_1 = require("./folder/topnavbar/topnavbar.component");
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'my-new-project';
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'app-root',
+            standalone: true,
+            templateUrl: './app.component.html',
+            styleUrl: './app.component.css',
+            imports: [common_1.CommonModule, router_1.RouterOutlet, my_component_component_1.MyComponentComponent, topnavbar_component_1.TopnavbarComponent]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
