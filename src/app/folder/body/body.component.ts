418dc06e07eba76ce745aed4cc038388d58d6f92
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-01-14 09:28:20 +0530

    wqwqwq

--- src/app/folder/body/body.component.ts
+++ src/app/folder/body/body.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
+  room: string; 
+  
+  constructor() {
+    this.room = "example room"; 
+  }
+  currentDate: string = new Date().toDateString(); 
+  availableRooms: number[] = [10, 12, 15, 14, 11, 13, 16]; 
 
+  proceed() {
+    // Add your logic for the proceed action here
+  }
 }
