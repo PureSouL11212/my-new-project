26a4260a3866390de1c461c298005be74b83d59b
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-01-14 08:03:41 +0530

    swdwer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { MyComponentComponent } from "./folder/my-component/my-component.component";
 import { TopnavbarComponent } from './folder/topnavbar/topnavbar.component';
 import { SecondnavbarComponent } from './folder/secondnavbar/secondnavbar.component';
+import { BodyComponent } from './folder/body/body.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, MyComponentComponent ,TopnavbarComponent, SecondnavbarComponent ]
+    imports: [CommonModule, RouterOutlet, MyComponentComponent ,BodyComponent,TopnavbarComponent, SecondnavbarComponent ]
 })
 export class AppComponent {
   title = 'my-new-project';
--- src/app/dist/app.component.js
+++ src/app/dist/app.component.js
@@ -13,6 +13,7 @@ var router_1 = require("@angular/router");
 var my_component_component_1 = require("./folder/my-component/my-component.component");
 var topnavbar_component_1 = require("./folder/topnavbar/topnavbar.component");
 var secondnavbar_component_1 = require("./folder/secondnavbar/secondnavbar.component");
+var body_component_1 = require("./folder/body/body.component");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.title = 'my-new-project';
@@ -23,7 +24,7 @@ var AppComponent = /** @class */ (function () {
             standalone: true,
             templateUrl: './app.component.html',
             styleUrl: './app.component.css',
-            imports: [common_1.CommonModule, router_1.RouterOutlet, my_component_component_1.MyComponentComponent, topnavbar_component_1.TopnavbarComponent, secondnavbar_component_1.SecondnavbarComponent]
+            imports: [common_1.CommonModule, router_1.RouterOutlet, my_component_component_1.MyComponentComponent, body_component_1.BodyComponent, topnavbar_component_1.TopnavbarComponent, secondnavbar_component_1.SecondnavbarComponent]
         })
     ], AppComponent);
     return AppComponent;
