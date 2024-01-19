35fb429666794c43bb10591d7965707c7a9fff56
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-01-19 12:02:08 +0530

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,9 @@ import { TopnavbarComponent } from './folder/topnavbar/topnavbar.component';
 import { SecondnavbarComponent } from './folder/secondnavbar/secondnavbar.component';
 import { BodyComponent } from './folder/body/body.component';
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full'},
-    { path: 'login', loadComponent: () => import('./folder/loginpage/loginpage.component').then(c => c.LoginpageComponent )},
-    {path:'Topnavbar',component:TopnavbarComponent},
-    {path:'second',component:SecondnavbarComponent},
-    {path:'body',component:BodyComponent},
-
-
-    
-
-    
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', loadComponent: () => import('./folder/loginpage/loginpage.component').then(c => c.LoginpageComponent) },
+    { path: 'Topnavbar', component: TopnavbarComponent },
+    { path: 'second', component: SecondnavbarComponent },
+    { path: 'body', component: BodyComponent },
 ];
