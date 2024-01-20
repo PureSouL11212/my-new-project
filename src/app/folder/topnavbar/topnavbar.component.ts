560bc3c52488b3d947b548fb8e7cccb76ff66e8e
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-01-20 11:24:35 +0530

    sasasa

--- src/app/folder/topnavbar/topnavbar.component.ts
+++ src/app/folder/topnavbar/topnavbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondnavbarComponent } from '../secondnavbar/secondnavbar.component';
 import { BodyComponent } from '../body/body.component';
+import { FooterComponent } from "../../footer/fotter.component";
 @Component({
-  selector: 'app-topnavbar',
-  standalone: true,
-  imports: [SecondnavbarComponent,BodyComponent],
-  templateUrl: './topnavbar.component.html',
-  styleUrl: './topnavbar.component.css'
+    selector: 'app-topnavbar',
+    standalone: true,
+    templateUrl: './topnavbar.component.html',
+    styleUrl: './topnavbar.component.css',
+    imports: [SecondnavbarComponent, BodyComponent, FooterComponent]
 })
 export class TopnavbarComponent {
 
