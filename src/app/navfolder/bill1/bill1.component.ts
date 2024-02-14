b169d7d77096f18e59fb1db8dba63cdf4df0f0d6
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-02-14 19:28:31 +0530

    wqwqwqw

--- src/app/navfolder/bill1/bill1.component.ts
+++ src/app/navfolder/bill1/bill1.component.ts
@@ -9,5 +9,27 @@ import { BurgerComponent } from '../../folder/burger/burger.component';
   styleUrl: './bill1.component.css'
 })
 export class Bill1Component {
-  billAmount = '';
+  bill: string = '';
+  showTable: boolean = false;
+  headings: string[] = ['Heading 1', 'Heading 2', 'Heading 3', 'Heading 4', 'Heading 5', 'Heading 6', 'Heading 7', 'Heading 8', 'Heading 9'];
+  tableData: any[][] = [];
+
+  generateTable() {
+    this.showTable = true;
+    this.tableData = [];
+    for (let i = 0; i < 6; i++) {
+      let row = [];
+      for (let j = 0; j < 9; j++) {
+        row.push(`Row ${i + 1}, Column ${j + 1}`);
+      }
+      this.tableData.push(row);
+    }
+  }
+
+  updateHeading(event: any, index: number) {
+    const target = event?.target;
+    if (target instanceof HTMLElement) {
+      this.headings[index] = target.innerText;
+    }
+  }
 }
